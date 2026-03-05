5cb83f7adbf1b0339635498f40024fa1d65b3637
Federico-CM <fcastro.biol@gmail.com>
2026-03-05 13:59:38 -0600

    Improve formating of the EDA script in the ifood analisis project

---
+++ projects/full_analisis_ifood/README.md
@@ -0,0 +1,6 @@
+# ifood data analisis (python)
+
+## What is this?
+This a full analisis project that works with the ifood dataset in order to investigate which factors affect the success of a marketing campaign.
+This includes all steps on the data analisis pipeline.
+
--- projects/full_analisis_ifood/eda.py
+++ projects/full_analisis_ifood/eda.py
@@ -9,6 +9,8 @@ from pathlib import Path
 import pandas as pd
 import matplotlib.pyplot as plt
 
+from scipy.stats import ttest_ind
+
 # -------------------
 # Load  the data
 # -------------------
@@ -17,32 +19,83 @@ CSV_PATH = BASE_DIR / "ml_project1_data.csv"
 df = pd.read_csv(CSV_PATH)
 df["Dt_Customer"] = pd.to_datetime(df["Dt_Customer"], errors="coerce")
 
+
 # -------------------
 # Basic sanity check
 # -------------------
 print("Shape:", df.shape)
 print("\nData types:\n", df.dtypes)
 
-# -------------------
-# Missing values
-# -------------------
-print("\nMissing values (%):")
-print((df.isna().mean() * 100).sort_values(ascending=False))
-
-# -------------------
-# Constant columns
-# -------------------
-print("\nColumns with only 1 unique value:")
-print(df.nunique()[df.nunique() == 1])
-
-# -------------------
-# Data range check
-# -------------------
 print("\nData range check:")
 print(df.select_dtypes(include='number').agg(['min', 'max']).T)
 
 # -------------------
-# Outlier detection (IQR method)
+# Data engineering
+# -------------------
+
+# Total Spend
+sales_cols = [
+    "MntWines",
+    "MntFruits",
+    "MntMeatProducts",
+    "MntFishProducts",
+    "MntSweetProducts"
+]
+df["TotalSpend"] = df[sales_cols].sum(axis=1)
+
+# Total Purchases
+purchases_cols = [
+    "NumWebPurchases",
+    "NumCatalogPurchases",
+    "NumStorePurchases"
+]
+df["TotalPurchases"] = df[purchases_cols].sum(axis=1)
+
+# Accepted any previous campaign
+campaign_cols = [
+    "AcceptedCmp1",
+    "AcceptedCmp2",
+    "AcceptedCmp3",
+    "AcceptedCmp4",
+    "AcceptedCmp5"
+]
+df['AcceptedAnyPrev'] = (df[campaign_cols].sum(axis=1) > 0).astype(int)
+
+
+# -------------------
+# 1- Missing values
+# -------------------
+print("\nMissing values (%):")
+print((df.isna().mean() * 100).sort_values(ascending=False))
+
+test_cols = [
+    "Year_Birth",
+    "Recency",
+    "TotalSpend",
+    "TotalPurchases",
+]
+
+results = (
+    df[test_cols]
+    .apply(lambda col: ttest_ind(
+        col[df["Income_missing"] == 0],
+        col[df["Income_missing"] == 1],
+        nan_policy="omit"
+    )[1])
+)
+print("\nt-test association of NA values:")
+print(results)
+
+
+# -------------------
+# 2- Constant columns
+# -------------------
+print("\nColumns with only 1 unique value:")
+print(df.nunique()[df.nunique() == 1])
+
+
+# -------------------
+# 3- Outlier detection (IQR method)
 # -------------------
 def detect_outliers(series):
     """A function to get outliers in a column"""
@@ -59,8 +112,9 @@ print(detect_outliers(df["Year_Birth"]))
 print("\nIncome outliers:")
 print(detect_outliers(df["Income"].dropna()).sort_values(ascending=False).head())
 
+
 # -------------------
-# Categorical inspection
+# 4- Categorical inspection
 # -------------------
 print("\nEducation categories:")
 print(df["Education"].value_counts())
@@ -68,8 +122,9 @@ print(df["Education"].value_counts())
 print("\nMarital_Status categories:")
 print(df["Marital_Status"].value_counts())
 
+
 # -------------------
-# Campaign acceptance distribution
+# 5- Campaign acceptance distribution
 # -------------------
 print("\nResponse distribution:")
 print(df["Response"].value_counts().sort_index())
@@ -81,18 +136,10 @@ plt.xlabel("Campaign response")
 plt.ylabel("Count")
 plt.show()
 
-# -------------------
-# Sales vs Campaign response
-# -------------------
-sales_cols = [
-    "MntWines",
-    "MntFruits",
-    "MntMeatProducts",
-    "MntFishProducts",
-    "MntSweetProducts"
-]
 
-df["TotalSpend"] = df[sales_cols].sum(axis=1)
+# -------------------
+# 6- Sales vs Campaign response
+# -------------------
 
 plt.figure()
 df.boxplot(column="TotalSpend", by="Response")
@@ -102,16 +149,10 @@ plt.xlabel("Response")
 plt.ylabel("Total Spend")
 plt.show()
 
-# -------------------
-# Total purchases vs Campaign response
-# -------------------
-purchases_cols = [
-    "NumWebPurchases",
-    "NumCatalogPurchases",
-    "NumStorePurchases"
-]
 
-df["TotalPurchases"] = df[purchases_cols].sum(axis=1)
+# -------------------
+# 7- Total purchases vs Campaign response
+# -------------------
 
 plt.figure()
 df.boxplot(column="TotalPurchases", by="Response")
@@ -121,24 +162,17 @@ plt.xlabel("Response")
 plt.ylabel("Total Purchases")
 plt.show()
 
-# -------------------
-# Past campaign analysis
-# -------------------
-campaign_cols = [
-    "AcceptedCmp1",
-    "AcceptedCmp2",
-    "AcceptedCmp3",
-    "AcceptedCmp4",
-    "AcceptedCmp5"
-]
 
-df['AcceptedAnyPrev'] = (df[campaign_cols].sum(axis=1) > 0).astype(int)
+# -------------------
+# 8- Past campaign analysis
+# -------------------
 
 print("Previous campaign acceptance vs. response cross-tabulation:")
 print(pd.crosstab(df['AcceptedAnyPrev'], df['Response'], normalize='index'))
 
+
 # -------------------
-# Age analysis (by decade cohorts)
+# 9- Age analysis (by decade cohorts)
 # -------------------
 df["BirthDecade"] = (df["Year_Birth"] // 10) * 10
 df[df["Response"] == 1].groupby("BirthDecade").size()
